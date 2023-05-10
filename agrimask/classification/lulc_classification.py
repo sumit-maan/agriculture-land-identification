2191d4804e5501f50c80bdebff4d461faee1ca23
sumitmaan <sumitmaansingh@gmail.com>
2023-05-10 15:54:38 +0530

    Modify the classification module name

--- agrimask/classification/lulc_classification.py
+++ agrimask/classification/lulc_classification.py
@@ -3,7 +3,7 @@ import os.path
 import faiss
 import numpy as np
 from osgeo import gdal
-from unsupcc.utils.raster import write_raster
+from agrimask.utils.raster import write_raster
 
 
 class Classifier:
@@ -11,11 +11,9 @@ class Classifier:
         self.n_init = 10
         self.max_iter = 300
 
-    def crop_cluster(self, layer_stack_path, date_band_ids, number_of_cluster=None, number_of_crop=None):
+    def lulc_cluster(self, layer_stack_path, date_band_ids, number_of_cluster=None):
         layer_stack_path = layer_stack_path.replace('//', '/')
         out_dir = '/'.join(os.path.splitext(layer_stack_path)[0].split('/')[:-2])
-        if not number_of_cluster and number_of_crop:
-            number_of_cluster = int(number_of_crop) + 3
         samples, original_layer_shape = self.multi_band_raster_to_array(layer_stack_path)
         sub_samples = [samples[:, i - 1] for i in date_band_ids]
         train_samples = np.column_stack(sub_samples)
@@ -29,7 +27,7 @@ class Classifier:
         label_arr = labels.reshape(original_layer_shape)
 
         del labels
-        outfile = os.path.join(out_dir, 'crop_cluster.tif')
+        outfile = os.path.join(out_dir, 'lulc_cluster.tif')
         outfile = outfile.replace('\\', '/')
         write_raster(layer_stack_path, label_arr, outfile, gdal.GDT_UInt16)
         del label_arr
